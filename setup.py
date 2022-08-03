42a4102a778446a0f5d40a135b08e19093ed6190
Praveen Venkatesh <praveenv253@gmail.com>
2022-08-02 17:49:29 -0700

    WIP: Add setup.py to make package installable.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='gpid',
+    version='0.1.0',
+    description='Algorithms for computing Partial Information Decompositions on Gaussian distributions',
+    author='Praveen Venkatesh',
+    url='https://github.com/praveenv253/gpid',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.5',
+    install_requires=['numpy', 'scipy', ],
+    #setup_requires=['pytest-runner', ],
+    #tests_require=['pytest', ],
+    license='MIT',
+)
